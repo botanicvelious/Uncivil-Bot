d59b85768c2d82d0cee2f57201e5b12e7f63d66b
botanicvelious <cyberempires@gmail.com>
2021-02-25 19:58:45 -0700

    Revert "async"
    This reverts commit f3b51a7d59719fbb1500106bea512df893ec94de.

--- test.py
+++
@@ -1,6 +0,0 @@
-from chat_downloader import ChatDownloader
-
-url = 'https://www.youtube.com/watch?v=frVrQ6NthAs'
-chat = ChatDownloader().get_chat(url)       # create a generator
-for message in chat:                        # iterate over messages
-    print(chat.format(message))             # print the formatted message
