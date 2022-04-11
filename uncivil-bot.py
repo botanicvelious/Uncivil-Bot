314c5e6f50bd9ce09dd813a39983c48a946eaacb
botanicvelious <cyberempires@gmail.com>
2022-04-11 04:31:55 -0600

    update url parser

--- uncivil-bot.py
+++ uncivil-bot.py
@@ -7,13 +7,49 @@ from functools import partial
 import os
 import string
 import random
-from pytchat.core_async.livechat import LiveChatAsync
+from pytchat import LiveChatAsync
+import pytchat
 import traceback
+from urllib.parse import urlparse, parse_qs
 
 token = open("token", "r").read()
 
 
 class MyClient(discord.Client):
+    def get_yt_video_id(self, url):
+        """Returns Video_ID extracting from the given url of Youtube
+        
+        Examples of URLs:
+          Valid:
+            'http://youtu.be/_lOT2p_FCvA',
+            'www.youtube.com/watch?v=_lOT2p_FCvA&feature=feedu',
+            'http://www.youtube.com/embed/_lOT2p_FCvA',
+            'http://www.youtube.com/v/_lOT2p_FCvA?version=3&amp;hl=en_US',
+            'https://www.youtube.com/watch?v=rTHlyTphWP0&index=6&list=PLjeDyYvG6-40qawYNR4juzvSOg-ezZ2a6',
+            'youtube.com/watch?v=_lOT2p_FCvA',
+          
+          Invalid:
+            'youtu.be/watch?v=_lOT2p_FCvA',
+        """
+        
+        if url.startswith(('youtu', 'www')):
+            url = 'http://' + url
+            
+        query = urlparse(url)
+        
+        if 'youtube' in query.hostname:
+            if query.path == '/watch':
+                doreturn = parse_qs(query.query)['v'][0]
+            elif query.path.startswith(('/embed/', '/v/')):
+                doreturn = query.path.split('/')[2]
+        elif 'youtu.be' in query.hostname:
+            doreturn = query.path[1:]
+        if doreturn.find("?") != -1:
+            urlstr = doreturn.split("?", 1)
+            return urlstr[0]
+        else:
+            return doreturn
+
     async def checkmessagesfunc(self, chatdata, message):
         channel = discord.utils.get(
             message.guild.channels, name="bot-for-questions")
@@ -32,9 +68,9 @@ class MyClient(discord.Client):
             elif c.message.lower().startswith("@uncivil law") or c.message.lower().startswith("question"):
                 message_id = await channel.send('''```'''+c.author.name+''':```''' + c.message)
                 await message_id.add_reaction(emoji)
-                await chatdata.tick_async()
-            else:
-                await chatdata.tick_async()
+            #else:
+            #    await channel.send('''```'''+c.author.name+''':```''' + c.message)
+            await chatdata.tick_async()
 
     async def dumpchat(self, message):
         res = ''.join(random.choices(
@@ -87,15 +123,21 @@ class MyClient(discord.Client):
 
             url = re.search(
                 "(?P<url>https?://[^\s]+)", message.content).group("url")
-            urlstr = url.split("=", 1)
-            urlstr = urlstr[1].split("&", 1)
+            urlstr = self.get_yt_video_id(url)
 
             await channel.send(f'Parsing chat for <{url}>')
 
-            livechat = LiveChatAsync(urlstr[0], callback=(
+            livechat = LiveChatAsync(str(urlstr), callback=(
                 lambda chatdata: self.checkmessagesfunc(chatdata, message)))
             while livechat.is_alive():
-                await asyncio.sleep(1)
+                await asyncio.sleep(3)
+            try:
+                livechat.raise_for_status()
+            except pytchat.ChatDataFinished:
+                await channel.send(f'Chat data finished.')
+            except Exception as e:
+                print(type(e), str(e))
+                await channel.send(str(e))
 
             #await channel.purge(limit=9000, bulk=False)
             await channel.send(f'Done parsing chat for <{url}> so channel cleared!')
@@ -113,11 +155,11 @@ class MyClient(discord.Client):
             return
 
         # check for !getchat
-        if re.search("(?P<url>https?://[^\s]+)", message.content) and message.content.lower().startswith("!getchat"):
+        if re.match(r"((http|https)\:\/\/(www.youtube.com\/watch\?v=|youtube.com\/watch\?v=))", message.content) and message.content.lower().startswith("!getchat"):
             self.bg_task = self.loop.create_task(self.dumpchat(message))
 
         # get questions
-        elif re.search("(?P<url>https?://[^\s]+)", message.content) and (message.channel.name == 'bot-for-questions' or message.channel.name == 'server-announcements'):
+        elif re.match(r"((http|https)\:\/\/(www.youtube.com\/watch\?v=|youtube.com\/watch\?v=))", message.content) and (message.channel.name == 'bot-for-questions' or message.channel.name == 'server-announcements'):
             self.bg_task = self.loop.create_task(self.background_task(message))
 
 
